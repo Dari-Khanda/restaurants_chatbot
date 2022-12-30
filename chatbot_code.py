0d982f1fd6816a5c9be4071e5090e01848d0a88e
Dari-Khanda <gdarikhanda@gmail.com>
2022-12-30 17:47:24 +0300

    get token

--- chatbot_code.py
+++ chatbot_code.py
@@ -1,2 +1,15 @@
+import telegram as telegram
 import numpy as np
 import pandas as pd
+from telegram.ext import Updater, CommandHandler
+
+Token = '5961342193:AAHSKbWve55wMb8Z6mNFq44tQCGDW6Trnjs'
+updater = Updater('5961342193:AAHSKbWve55wMb8Z6mNFq44tQCGDW6Trnjs', use_context='True')
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    update.message.reply_text('This is a restaurant advisor bot')
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.start_polling()
